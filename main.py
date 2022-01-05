fe9acfdfb160b010eba586ababa082f130351caf
Igor <32133421+IgorAnw@users.noreply.github.com>
2022-01-05 19:08:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import pygame
 import sys
 from player import Character
-from constants import WIDTH, HEIGHT
-from platform import Platform
+from constants import *
+from brick import Brick
 from enemy import Enemy
 
 player_group = pygame.sprite.Group()
@@ -14,27 +14,34 @@ pygame.init()
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
 player = Character(player_group)
 enemy = Enemy(enemy_group)
-obstacle = Platform(obstacles_group)
-obstacle1 = Platform(obstacles_group)
+for i in range(38):
+    obstacle = Brick(obstacles_group, 100 + i * BRICK_SIZE, 500)
+for i in range(10):
+    obstacle = Brick(obstacles_group, 100 + i * BRICK_SIZE, 200)
+for i in range(10):
+    obstacle = Brick(obstacles_group, 500 + i * BRICK_SIZE, 200)
 
 while True:
     screen.fill('black')
     player_group.draw(screen)
     obstacles_group.draw(screen)
     enemy_group.draw(screen)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_z:
+                player.jump()
 
     if pygame.key.get_pressed()[pygame.K_RIGHT]:
         player.move(1)
     if pygame.key.get_pressed()[pygame.K_LEFT]:
         player.move(-1)
-    if pygame.key.get_pressed()[pygame.K_z]:
-        player.jump()
 
     enemy.move(pygame.sprite.spritecollideany(enemy, obstacles_group))
     player.update(pygame.sprite.spritecollideany(player, obstacles_group))
     clock.tick(60)
+
     pygame.display.flip()
